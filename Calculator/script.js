3f974d8e8c84563492364f992e950d76ddbfd5f3
Galang <162521324+IGALAN9@users.noreply.github.com>
2024-09-02 18:46:45 +0700

    First commit

---
+++ Calculator/script.js
@@ -0,0 +1,12 @@
+function add(id,inp1,inp2) {
+    document.getElementById(id).value = Number(inp1) + Number(inp2);
+}
+function sub(id,inp1,inp2) {
+    document.getElementById(id).value = Number(inp1) - Number(inp2);
+}
+function time(id,inp1,inp2) {
+    document.getElementById(id).value = Number(inp1) * Number(inp2);
+}
+function dived(id,inp1,inp2) {
+    document.getElementById(id).value = Number(inp1) / Number(inp2);
+}
